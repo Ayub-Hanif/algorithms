7f0b6463eb145833b195225c71670cc6db82a6ee
Ayub-Hanif <69708555+Ayub-Hanif@users.noreply.github.com>
2021-03-17 14:19:11 -0700

    string finder algorithm
    This algorithm can find the most frequently occurred string a vector.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+std::string More_repeated(std::vector<std::string> Names, int n)
+{
+    std::string mode;
+    std::vector<std::string> repeated_ones;
+    std::vector<int> num_repeted;
+    num_repeted.assign(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        repeated_ones.push_back(Names[i]);
+    }
+    for (int z = 0; z < n; z++)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if (Names[i] == repeated_ones[z])
+            {
+                num_repeted.at(i) += 1;
+            }
+            if (num_repeted[i] > num_repeted[z])
+            {
+                mode = repeated_ones[i];
+            }
+        }
+    }
+    return mode;
+}
+
+int main()
+{
+    std::vector<std::string> Names = { "msft" , "appl", "appl" ,"msft", "appl" ,"msft" };
+    int n = 6;
+    std::string mode;
+
+    mode = More_repeated(Names, n);
+    std::cout << mode;
+}
